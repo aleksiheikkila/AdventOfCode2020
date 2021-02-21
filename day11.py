09b22a073b9aa48bb5e2c3ee358f7263f149d1fb
Aleksi Heikkila <aleksi.j.heikkila@outlook.com>
2021-02-21 15:42:05 +0200

    day11 cleaned

--- day11.py
+++ day11.py
@@ -17,8 +17,7 @@ OCCUPIED = "#"
 
 # floor (.), an empty seat (L), or an occupied seat (#)
 
-
-# Rules
+# Rules:
 # (one of the eight positions immediately up, down, left, right, or diagonal from the seat)
 
 # If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
@@ -26,7 +25,6 @@ OCCUPIED = "#"
 # Otherwise, the seat's state does not change.
 
 # Floor (.) never changes; seats don't move, and nobody sits on the floor.
-
 
 def get_adjs_counts(grid, pos):
     row, col = pos
@@ -71,7 +69,7 @@ def next_state(grid, pos, method="adjs"):
         adj_counts = get_adjs_counts(grid, pos)
         if grid[row][col] == OCCUPIED and adj_counts["#"] >= 4:
             return EMPTY
-    #print(adj_counts)
+
     if method == "visible":
         adj_counts = get_visible_counts(grid, pos)
         if grid[row][col] == OCCUPIED and adj_counts["#"] >= 5:
@@ -79,8 +77,6 @@ def next_state(grid, pos, method="adjs"):
     
     if grid[row][col] == EMPTY and adj_counts["#"] == 0:
         return OCCUPIED
-    #if grid[row][col] == OCCUPIED and adj_counts["#"] >= 4:
-    #    return EMPTY
 
     return grid[row][col]
 
@@ -95,7 +91,6 @@ def step(grid, method="adjs"):
     for row in range(rows):
         for col in range(cols):
             new_grid[row][col] = next_state(grid, (row, col), method=method)
-            #print(f"@{row}{col}: {new_grid[row][col]} ( old: {grid[row][col]})")
 
     return new_grid
 
@@ -108,10 +103,8 @@ def step_until_stable(grid, method="adjs"):
     curr_grid = grid
     while True:
         new_grid = step(curr_grid, method)
-
         if new_grid == curr_grid:
             return new_grid
-
         curr_grid = new_grid
 
 
@@ -129,7 +122,6 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL"""
 
-
 grid_test = [[el for el in row.strip()] for row in RAW_GRID_TEST.split("\n")]
 
 # Part A
@@ -142,13 +134,11 @@ assert count_occupied(step_until_stable(grid_test, method="visible")) == 26
 ###
 # Problem
 ###
-
 with open("./inputs/day11.txt") as f:
     grid = [[el for el in row.strip()] for row in f.read().split("\n")]
 
 # Part A:
 print("Part A:", count_occupied(step_until_stable(grid, method="adjs")))
-
 
 # Part B:
 print("Part B:", count_occupied(step_until_stable(grid, method="visible")))
